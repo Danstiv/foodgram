30cf2e5001211d9213755381902bec177f87012d
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-08-08 13:18:17 +0700

    Update django settings
    Now default db is PostgreSQL, debugging is turned off, and secret key is loaded from env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,12 +4,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-SECRET_KEY = 'q(%ng$f3l(=xbe$t2_t!xa8^oipn)4=aja8gw=8pqsvmq667tm'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['web']
-
 
 
 INSTALLED_APPS = [
@@ -60,8 +59,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres_password'),
+        'HOST': os.getenv('DB_HOST', 'db_host'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
